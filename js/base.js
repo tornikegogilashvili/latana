5fbb7479a9d92249ed99937b3456355ee584290d
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-02-05 20:00:25 +0400

    latana

--- js/base.js
+++ js/base.js
@@ -12,7 +12,7 @@ const getInformation = async () => {
             `
             <section class="menu_section" id="${info.id}">
                 
-                <a href="enter.html"><div class="menu_section_div"><img src="${info.images[0]}" alt="latana"></div></a>
+                <div class="menu_section_div"><img src="${info.images[0]}" alt="latana"></div>
                 <div class="menu_section_div_2">
                     <p class="menu_section_category">${info.category}</p>
                     <h3 class="menu_section_title">${info.title}</h3>
@@ -46,9 +46,15 @@ const getInformation = async () => {
                 `
                 <div class="enter_main_div" id="enter_main_div">
                     <h3 class="enter_main_title">${users.products[e.id-1].title}</h3>
-                    
+                        
                     <p class="enter_main_text">${users.products[e.id-1].description}</p>
-                       
+                       <div class= "article_latestnews_div_person">
+                            <img src="images/profile_photo.jpg" alt="profile picture">
+                            <div class="article_latestnews_div_name">
+                                <p class="article_latestnews_name">George Thomas</p>
+                                <p class="article_latestnews_date">22.01.1996</p>
+                            </div>
+                        </div>
                     <div class="enter_main_img_div"><img class="enter_main_img" src="${users.products[e.id-1].images[0]}" alt="latana"></div>
                     <p class="enter_main_text">${users.products[e.id-1].description}</p>
                     <div class="enter_main_little_div">
